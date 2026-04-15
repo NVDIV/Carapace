f99daa02a2a70587316fdd222fbf0b6186b9e932
Nadiia Rybak <nvdiv5@gmail.com>
2026-04-15 08:09:40 +0300

    feat(command): new commands

--- src/interpreter.py
+++ src/interpreter.py
@@ -1,4 +1,4 @@
-from src.parser import ASTNode, ForwardNode, LeftNode, RepeatNode
+from src.parser import ASTNode, ForwardNode, LeftNode, RepeatNode, BackwardNode, RightNode, PenUpNode, PenDownNode, ColorNode, WidthNode, SpeedNode
 import src.commands as commands
 from src.errors import RuntimeError
 
@@ -27,8 +27,14 @@ class Interpreter:
             case ForwardNode(distance=d):
                 commands.execute_forward(d)
 
+            case BackwardNode(d): 
+                commands.execute_backward(d)
+
             case LeftNode(angle=a):
                 commands.execute_left(a)
+
+            case RightNode(a): 
+                commands.execute_right(a)
 
             case RepeatNode(times=t, body=b):
                 # Loop logic: repeat 't' times
@@ -36,6 +42,21 @@ class Interpreter:
                     # Recursively execute each child node in the loop body
                     for child_node in b:
                         self.execute(child_node)
+
+            case PenUpNode(): 
+                commands.execute_penup()
+
+            case PenDownNode(): 
+                commands.execute_pendown()
+
+            case ColorNode(c): 
+                commands.execute_color(c)
+
+            case WidthNode(w): 
+                commands.execute_width(w)
+
+            case SpeedNode(s): 
+                commands.execute_speed(s)
 
             case _:
                 raise RuntimeError(f"Unknown AST node: {node}")
